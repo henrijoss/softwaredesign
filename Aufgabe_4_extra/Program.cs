4d31c417a764774b8c681872fdcd784a1e1e83bc
henrijoss <44018513+henrijoss@users.noreply.github.com>
2018-11-18 17:47:17 +0100

    Add files via upload

--- Aufgabe_4_extra/Program.cs
+++ Aufgabe_4_extra/Program.cs
@@ -9,29 +9,29 @@ namespace Aufgabe_4_extra
         {
             Console.WriteLine("Geben Sie einen Satz ein:");
             string sentence = Console.ReadLine();
-            Console.WriteLine(ReverseWords(sentence));
+            Console.WriteLine(ReverseSentence(sentence));
 
         }
-        public static string ReverseChar(string sentence)
+        public static string ReverseLetters(string sentence)
         {   
             char[] charArray = sentence.ToCharArray();
             Array.Reverse(charArray);
             return new string(charArray);
         }
-        public static string ReverseSentence(string sentence) 
+        public static string ReverseWords(string sentence) 
         {
             string[] words = sentence.Split(' ');
             Array.Reverse(words);
             sentence = String.Join(" ", words);
             return sentence;
         }
-        public static string ReverseWords(string sentence) 
+        public static string ReverseSentence(string sentence) 
         {
             string[] words = sentence.Split(' ');
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < words.Length; i++) 
             {
-                sb.Append(ReverseChar(words[i]));
+                sb.Append(ReverseLetters(words[i]));
                 sb.Append(" ");            
             }
             return sb.ToString();
